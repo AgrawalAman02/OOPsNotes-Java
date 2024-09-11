66684cc1b919fb03576dae6fc68c4bd56c76c151
Aman Agrawal <154336147+AgrawalAman02@users.noreply.github.com>
2024-09-11 23:21:16 +0530

    Create Q5.java(Only Question. Code is still pending)
    Q.5. Write a java class which consists of 5 integer data. Overload constructor
    (Default &amp; normal) to initialize those integer data members. Provide a method which
    sorts those integer data members using bubble sort.

---
+++ Lab Assignment/Day4/Q5.java
@@ -0,0 +1,6 @@
+/*
+Q.5. Write a java class which consists of 5 integer data. Overload constructor
+(Default &amp; normal) to initialize those integer data members. Provide a method which
+sorts those integer data members using bubble sort.
+*/
+
